c9fd1e78cf946f0ff341cd946b8b825072182d13
Shriya75 <shriya@farmwiseai.com>
2023-09-26 16:10:53 +0530

    Commit

--- app/src/main/java/com/farmwiseai/demoapp/activity/DetailActivity.java
+++ app/src/main/java/com/farmwiseai/demoapp/activity/DetailActivity.java
@@ -1,16 +1,23 @@
 package com.farmwiseai.demoapp.activity;
+
 import com.farmwiseai.demoapp.R;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.widget.TextView;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import android.os.Bundle;
+import android.view.MenuItem;
 
 public class DetailActivity extends AppCompatActivity {
 
     private TextView usernameTextView, passwordTextView;
     private SharedPreferences sharedPreferences;
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +29,39 @@ public class DetailActivity extends AppCompatActivity {
 
         sharedPreferences = getSharedPreferences("login", MODE_PRIVATE);
 
-
         // Retrieve and display the saved username and password
         String username = sharedPreferences.getString("username", "");
         String password = sharedPreferences.getString("password", "");
 
         usernameTextView.setText("Username: " + username);
         passwordTextView.setText("Password: " + password);
+
+        // drawer layout instance to toggle the menu icon to open
+        // drawer and back button to close drawer
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    // override the onOptionsItemSelected()
+    // function to implement
+    // the item click listener callback
+    // to open and close the navigation
+    // drawer when the icon is clicked
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
